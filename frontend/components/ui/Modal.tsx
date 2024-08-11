b8f8f6779aec32df4cc13b5a6071d66006551d95
RizinGH <rizi.n2021@gmail.com>
2024-08-11 20:46:45 +0530

    patch-2

--- frontend/components/ui/Modal.tsx
+++ frontend/components/ui/Modal.tsx
@@ -50,7 +50,7 @@ export default function Modal({ children, Title, onClose, isOpen, buttonText, lo
                                     <Label className="text-xl font-semibold text-black dark:text-white">
                                         {Title}
                                     </Label>
-                                    <Button type='button' onClick={handleClose} className="w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
+                                    <Button type='button' onClick={handleClose} className="flex justify-center items-center" data-modal-hide="default-modal">
                                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                         </svg>
